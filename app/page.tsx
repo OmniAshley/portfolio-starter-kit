94a3fe99708db992e4183e341f0d77a251befc14
OmniAshley <omniashleybeebakee@gmail.com>
2025-09-20 22:51:20 +0100

    test: updated the description

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,9 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`This is Ashley Beebakee, I think it's about time that I
+        showcased my work and projects here. Feel free to explore
+        and get in touch!`}
       </p>
       <div className="my-8">
         <BlogPosts />
